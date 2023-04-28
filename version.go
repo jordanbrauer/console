a1a866fd6a0c311f26436f4450083d6c6fbb21b4
jordanbrauer <18744334+jordanbrauer@users.noreply.github.com>
2023-04-28 11:52:39 -0500

    fix typo

--- version.go
+++ version.go
@@ -4,7 +4,7 @@ import "fmt"
 
 var version = &Command{
 	Name:        "version",
-	Description: "Show the currently installed version of Mist",
+	Description: "Show the currently installed version",
 	Run: func(command *Command) ExitCode {
 		fmt.Println(command.app.Version)
 
